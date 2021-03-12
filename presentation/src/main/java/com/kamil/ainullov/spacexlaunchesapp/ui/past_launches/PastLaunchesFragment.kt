a2fd89c6299307f561fe9eb8fd82f2ad3d0f3f75
Kema <kainullov@gmail.com>
2021-03-12 09:18:02 +0300

    added status processing in past launches

--- presentation/src/main/java/com/kamil/ainullov/spacexlaunchesapp/ui/past_launches/PastLaunchesFragment.kt
+++ presentation/src/main/java/com/kamil/ainullov/spacexlaunchesapp/ui/past_launches/PastLaunchesFragment.kt
@@ -1,6 +1,7 @@
 package com.kamil.ainullov.spacexlaunchesapp.ui.past_launches
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.fragment.app.viewModels
 import com.kamil.ainullov.spacexlaunchesapp.R
 import com.kamil.ainullov.spacexlaunchesapp.base.BaseFragment
 import com.kamil.ainullov.spacexlaunchesapp.databinding.FragmentPastLaunchesBinding
+import com.kamil.ainullov.spacexlaunchesapp.utils.state.State
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,6 +36,26 @@ class PastLaunchesFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setActionBarData(getString(R.string.past_launches), true)
+        observeStates()
+        viewModel.getPastLaunches()
+    }
+
+    private fun observeStates() {
+        viewModel.pastLaunchesState.observe(viewLifecycleOwner, { state ->
+            when (state) {
+                is State.Default -> {
+                }
+                is State.Loading -> {
+                    binding.pbLoading.visibility = View.VISIBLE
+                }
+                is State.Success -> {
+                    binding.pbLoading.visibility = View.GONE
+                    state.data // can use
+                }
+                is State.Error -> {
+                }
+            }
+        })
     }
 
 }
--- presentation/src/main/java/com/kamil/ainullov/spacexlaunchesapp/ui/past_launches/PastLaunchesViewModel.kt
+++ presentation/src/main/java/com/kamil/ainullov/spacexlaunchesapp/ui/past_launches/PastLaunchesViewModel.kt
@@ -1,12 +1,35 @@
 package com.kamil.ainullov.spacexlaunchesapp.ui.past_launches
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.kamil.ainullov.domain.core.Result
+import com.kamil.ainullov.domain.entity.SimpleLaunchEntity
 import com.kamil.ainullov.domain.usecase.GetPastLaunchesUseCase
 import com.kamil.ainullov.spacexlaunchesapp.base.BaseViewModel
+import com.kamil.ainullov.spacexlaunchesapp.utils.ext.default
+import com.kamil.ainullov.spacexlaunchesapp.utils.ext.post
+import com.kamil.ainullov.spacexlaunchesapp.utils.ext.set
+import com.kamil.ainullov.spacexlaunchesapp.utils.state.State
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PastLaunchesViewModel @Inject constructor(
     private val getPastLaunchesUseCase: GetPastLaunchesUseCase
 ): BaseViewModel() {
+
+    private val _pastLaunchesState = MutableLiveData<State<List<SimpleLaunchEntity>>>().default(initialValue = State.Default)
+    val pastLaunchesState: LiveData<State<List<SimpleLaunchEntity>>> = _pastLaunchesState
+
+    fun getPastLaunches() {
+        _pastLaunchesState.set(newValue = State.Loading)
+        viewModelScope.launch {
+            when (val result = getPastLaunchesUseCase.invoke(Unit)) {
+                is Result.Success -> _pastLaunchesState.post(State.Success(result.data))
+                is Result.Error -> _pastLaunchesState.post(State.Error(result.failure))
+            }
+        }
+    }
 }
